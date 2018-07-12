c902894a79a02ffc27ff154ce5c76aa8dbfbfcc2
Vitaliy Cherlenevskyy <ch_v@ukr.net>
2018-07-12 00:26:31 +0000

    removed clearfix

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,8 +2,8 @@ document.querySelector('.overlay').addEventListener('click', closeNav)
 document.querySelector('.burger-button').addEventListener('click', openNav)
 
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
+    document.querySelector('#myNav').style.height = "100%";
 }
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
+    document.querySelector('#myNav').style.height = "0";
 }
